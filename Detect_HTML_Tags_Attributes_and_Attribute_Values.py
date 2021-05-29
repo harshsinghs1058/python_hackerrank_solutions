e1286b463bafa2c03545c6afc7a7541ca810c4e8
Harshsinghs1058 <harshsinghs1058@gmail.com>
2021-05-29 19:24:50 +0530

    detecting html tags

---
+++ Detect_HTML_Tags_Attributes_and_Attribute_Values.py
@@ -0,0 +1,18 @@
+# This code is written by harsh.
+from html.parser import HTMLParser
+
+# create a subclass and override the handler methods
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print(tag)
+        for a in attrs:
+            print(f"-> {a[0]} > {a[1]}")
+
+
+# instantiate the parser and fed it some HTML
+if __name__ == "__main__":
+    n = int(input())
+    parser = MyHTMLParser()
+    for _ in range(n):
+        s = input()
+        parser.feed(s)
